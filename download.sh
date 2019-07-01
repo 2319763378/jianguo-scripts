fc8ac7efd0f316ce369477d4a2c2fdce5ff4ecd2
2319763378 <41320010+2319763378@users.noreply.github.com>
2019-07-01 11:00:55 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,4 +3,5 @@ filter=$(curl -s $1 |grep -E "hash:|name:")
 hash1=$(echo ${filter}|awk -F"'" '{print $2}')
 name1=$(echo ${filter}|awk -F"'" '{print $4}')
 url=$(curl -s "https://www.jianguoyun.com/d/ajax/fileops/pubFileLink?k=${hash1}&name=${name1}&wm=false&forwin=1"|awk -F'"' '{print $4}')
+yum install wget -y
 wget -O ${name1} ${url}
